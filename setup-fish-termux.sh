72e40fa82c33664856ac332f6ed0cc2bd13d746b
Flora Maxine <101367730+FloMaskine@users.noreply.github.com>
2025-06-25 10:13:09 -0300

    Create setup-fish-termux.sh
    Created file o install fish terminal, set it as default and append basic aliases in the config.fish file.

---
+++ setup-fish-termux.sh
@@ -0,0 +1,34 @@
+##Fish install and setup
+#!/bin/bash
+
+# Install Fish
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt-get update
+sudo apt-get install fish
+
+# Set Fish as default (optional)
+echo /usr/local/bin/fish | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/fish
+
+echo "Adicionando Aliases"
+"
+alias upd="apt update && apt upgrade && pkg update && pkg upgrade"
+alias aliasconfig="nano ~/.config/fish/config.fish"
+alias instale="apt install"
+alias la="ls -a"
+alias rst="cd ~"
+alias cc="clear"
+alias ubuntu="proot-distro login ubuntu"
+alias llm="bash ~/Termux-doomsday-LLM/run.sh"
+alias start-alpine="qemu-system-x86_64 -machine q35 -m 1024 -smp cpus=2 -cpu qemu64 \
+  -drive if=pflash,format=raw,read-only,file=$PREFIX/share/qemu/edk2-x86_64-code.fd \
+  -netdev user,id=n1,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 \
+  -nographic alpine.img"
+
+clear
+" >> ~/.config/fish/config.fish
+
+
+echo "Restarting"
+reset
+clear
